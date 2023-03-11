c458e39f4966d52afc528fb9080408c70d3c7d15
Reza <r@r.com>
2023-03-11 09:59:07 +0330

    ss

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
 
-let matn = document.querySelector("#matn");
-const matnFunc = function () {
-  matn.textContent = Math.trunc(Math.random() * 1000) + 1;
-};
-//// lol
+let reza = 20;
+switch (reza) {
+  case 20:
+    console.log("hi");
+    break;
+}
